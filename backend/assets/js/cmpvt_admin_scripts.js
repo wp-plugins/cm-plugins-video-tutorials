5cf979d39b1a38277ef3ea07334281ddae89a8d1
CreativeMindsSolutions <CreativeMindsSolutions@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-18 13:36:06 +0000

    replace version
    git-svn-id: https://plugins.svn.wordpress.org/cm-plugins-video-tutorials/trunk@1162685 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- backend/assets/js/cmpvt_admin_scripts.js
+++
@@ -1,8 +0,0 @@
-
-(function ($) {
-    $(document).ready(function ($) {
-        $('select').on('change', function(){
-            $(this).parents('form').submit();
-        });
-    });
-})(jQuery);
